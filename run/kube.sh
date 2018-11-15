3988de3ab054dae6bf6350f27b0100080f8e0f72
Lucas Basquerotto <lucasbasquerotto>
2018-11-15 20:29:49 -0200

    K8s commands

---
+++ run/kube.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+ssh ubuntu@master-001.kube
+
+kubectl get nodes
+
+kubectl run nginx --image=nginx --port 80
+
+kubectl expose deploy nginx --port 80 --target-port 80 --type NodePort
+
+kubectl get services
+
+kubectl delete service nginx
+
+kubectl get services
+
+kubectl delete deployment nginx
+
+kubectl get deployments

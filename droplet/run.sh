fa55006a139d47f55d67ec4fb5daeb4b35beff7b
Lucas Basquerotto <lucasbasquerotto>
2018-12-08 12:13:16 -0200

    ansible do

--- droplet/run.sh
+++ droplet/run.sh
@@ -3,15 +3,19 @@ set -e
 
 GIT_REPO="https://github.com/lucasbasquerotto/ansible-demo.git"
 
+cd ~
+
 rm -rf ansible-demo
 rm -rf ansible
 
-cd ~
 git clone "$GIT_REPO"
 mkdir ansible
 shopt -s dotglob
 mv ansible-demo/droplet/* ansible/
 rm -rf ansible-demo
+
+mkdir ~/env
+mv ~/ansible/env.yml ~/env/env.yml
 
 # ansible-playbook ~/ansible/droplet.yml
 cd ~/ansible/

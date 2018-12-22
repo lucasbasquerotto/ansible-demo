df0c2f6a792195b993cbdf905662e6dde5e80a31
Lucas Basquerotto <lucasbasquerotto@gmail.com>
2018-12-21 22:00:21 -0200

    kube

--- run/kube.sh
+++ run/kube.sh
@@ -5,17 +5,20 @@ ssh ubuntu@master-001.kube
 
 kubectl get nodes
 
-kubectl run nginx --image=nginx --port 80
+#kubectl run nginx --image=nginx --port 80
+kubectl create deployment my-nginx --image nginx
 
-kubectl expose deploy nginx --port 80 --target-port 80 --type NodePort
+#kubectl expose deploy nginx --port 80 --target-port 80 --type NodePort
+#kubectl expose deployment my-nginx --type=LoadBalancer --port=8080
+kubectl expose deployment my-nginx --type=LoadBalancer --port 80 --target-port 80
 
 kubectl get services
 
-kubectl delete service nginx
+kubectl delete service my-nginx
 
 kubectl get services
 
-kubectl delete deployment nginx
+kubectl delete deployment  my-nginx
 
 kubectl get deployments
 
